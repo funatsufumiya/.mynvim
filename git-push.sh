45e330d95124f74adb5c5a4883dc71642b74f17a
Atmarksharp <funatsu.fumiya@gmail.com>
2015-10-02 12:23:38 +0900

    Update git-push.sh

--- git-push.sh
+++ git-push.sh
@@ -3,7 +3,5 @@
 if [ $# -eq 0 ]; then
   echo "Usage: ./git-push.sh [commit message]"
 else
-  git add --all
-  git commit -m "$1"
-  git push -u origin master
+  git add --all && git commit -m "$1" && git push -u origin master
 fi
