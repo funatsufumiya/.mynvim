64d5b907d1472116f8b2f5596f4a70c280cd3ce2
atmarksharp <funatsu.fumiya@gmail.com>
2016-08-01 23:56:43 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+echo "Install neobundle.vim"
 mkdir -p ~/.vim/bundle
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
 # Backup vimrc
+echo "Backup vimrc and gvimrc"
 cp ~/.vimrc ~/.vimrc.old
 cp ~/.gvimrc ~/.gvimrc.old
 
 # Replace vimrc to mine
+echo "Replace vimrc and gvimrc to ~/.myvim/vimrc"
 rm -f ~/.vimrc ~/.gvimrc
 ln -s ~/.myvim/vimrc ~/.vimrc
 ln -s ~/.myvim/vimrc ~/.gvimrc
 
 # Apply colors
+echo "Copying ~/.myvim/color/ to ~/.vim/colors/"
 mkdir -p ~/.vim/colors
 cp ~/.myvim/color/* ~/.vim/colors/
 
